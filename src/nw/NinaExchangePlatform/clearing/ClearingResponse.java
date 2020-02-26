495d31e7014135ee6ff1738a4b54cde160d268e2
sonny51261_local <sonny51261@gmail.com>
2020-02-25 20:46:13 -0800

    Updates to ClearingEngine

---
+++ src/nw/NinaExchangePlatform/clearing/ClearingResponse.java
@@ -0,0 +1,9 @@
+package nw.NinaExchangePlatform.clearing;
+
+public class ClearingResponse {
+
+    ClearingStatus ClearTrade(int marketParticipantID_A, int marketParticipantID_B){
+        return ClearingStatus.CLEARED;
+    }
+
+}
