7d029e2b529b6b8f019a6cf56f9acdcc9ecce9e8
NinaWang <niannian1989@gmail.com>
2020-03-09 12:23:38 -0700

    bug fix for function FillMarketOrder; Finish implementing function FillLimitOrder;

--- src/nw/ExchangePlatform/trading/TradingEngine.java
+++ src/nw/ExchangePlatform/trading/TradingEngine.java
@@ -64,26 +64,26 @@ public class TradingEngine {
 
     private boolean FillMarketOrder(MarketParticipantOrder order, ArrayList<MarketParticipantOrder> counterPartyLimitOrderBook, ArrayList<Transaction> transactions,
                                     ArrayList<UnfilledOrder> unfilledOrders) {
-        boolean active = false;
+        boolean active;
 
         if(counterPartyLimitOrderBook == null  || !CheckTradeViability(order, counterPartyLimitOrderBook.get(0))) {
             UnfilledOrder unfilled = new UnfilledOrder(order, "Could not match market order price");
             unfilledOrders.add(unfilled);
-            return active;
-        }
+            active = false;
 
-        MarketParticipantOrder topCounterLimitOrder = counterPartyLimitOrderBook.get(0);
-        boolean foundCounterParticipant = CheckTradeViability(order, counterPartyLimitOrderBook.get(0));
+        } else {
+            MarketParticipantOrder topCounterLimitOrder = counterPartyLimitOrderBook.get(0);
 
-        if(foundCounterParticipant) {
             double transactionPrice = topCounterLimitOrder.price;
             int transactionSize;
 
             if(order.size == topCounterLimitOrder.size) {
                 transactionSize = order.size;
                 counterPartyLimitOrderBook.remove(0);
+                active = false;
             } else if (order.size < topCounterLimitOrder.size) {
                 transactionSize = order.size;
+                active = false;
             } else {
                 transactionSize = topCounterLimitOrder.size;
                 order.size -= transactionSize;
@@ -91,13 +91,13 @@ public class TradingEngine {
                 active = true;
             }
 
-            Transaction counterSideTransaction = new Transaction(topCounterLimitOrder.userID, topCounterLimitOrder.name, previousTransactionID +1, topCounterLimitOrder.orderID, new Date(),
+            Transaction counterPartyTransaction = new Transaction(topCounterLimitOrder.userID, topCounterLimitOrder.name, previousTransactionID +1, topCounterLimitOrder.orderID, new Date(),
                     topCounterLimitOrder.direction, topCounterLimitOrder.tickerSymbol, transactionSize, transactionPrice);
             Transaction currentOrderTransaction = new Transaction(order.userID, order.name, previousTransactionID +2, order.orderID, new Date(),
                     order.direction, order.tickerSymbol, transactionSize, transactionPrice);
 
             transactions.add(currentOrderTransaction);
-            transactions.add(counterSideTransaction);
+            transactions.add(counterPartyTransaction);
             previousTransactionID += 2;
         }
         return active;
@@ -105,7 +105,7 @@ public class TradingEngine {
 
     private boolean FillLimitOrder(MarketParticipantOrder order, ArrayList<MarketParticipantOrder> currentLimitOrderBook, ArrayList<MarketParticipantOrder> counterPartyLimitOrderBook, ArrayList<Transaction> transactions,
                                    ArrayList<UnfilledOrder> unfilledOrders, ArrayList<PendingOrder> pendingOrders) {
-        boolean active = false;
+        boolean active;
 
         //If no match is found, add order to limit order book, return pending message to participant
         if(counterPartyLimitOrderBook == null  || !CheckTradeViability(order, counterPartyLimitOrderBook.get(0))) {
@@ -115,22 +115,21 @@ public class TradingEngine {
 
             PendingOrder pendingOrder = new PendingOrder(order, "Your order has been processed. Will notify you when we find a match");
             pendingOrders.add(pendingOrder);
-            return active;
-        }
+            active = false;
 
-        //If there is a match, match order, put remaining in limit order book if possible
-        MarketParticipantOrder topCounterLimitOrder = counterPartyLimitOrderBook.get(0);
-        boolean foundCounterParticipant = CheckTradeViability(order, counterPartyLimitOrderBook.get(0));
-
-        if(foundCounterParticipant) {
+        } else {
+            //If there is a match, match order, put remaining in limit order book if possible
+            MarketParticipantOrder topCounterLimitOrder = counterPartyLimitOrderBook.get(0);
             double transactionPrice = topCounterLimitOrder.price;
             int transactionSize;
 
             if(order.size == topCounterLimitOrder.size) {
                 transactionSize = order.size;
                 counterPartyLimitOrderBook.remove(0);
+                active = false;
             } else if (order.size < topCounterLimitOrder.size) {
                 transactionSize = order.size;
+                active = false;
             } else {
                 transactionSize = topCounterLimitOrder.size;
                 order.size -= transactionSize;
@@ -151,11 +150,13 @@ public class TradingEngine {
     }
 
     private boolean CheckTradeViability(MarketParticipantOrder order, MarketParticipantOrder counterLimitOrder) {
-        boolean viable = false;
+        boolean viable;
 
         if((order.direction == Direction.BUY && order.price >= counterLimitOrder.price)
             || (order.direction == Direction.SELL && order.price <= counterLimitOrder.price)) {
             viable = true;
+        } else {
+            viable = false;
         }
         return viable;
     }
