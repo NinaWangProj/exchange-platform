dfa8e9ab1c8bee614c9eb28422db0bbae9cf8515
NinaWang <niannian1989@gmail.com>
2020-05-27 12:09:07 -0700

    create OrderDTO class to serialize and deserialize orders

--- src/nw/ExchangePlatform/client/ExchangeClient.java
+++ src/nw/ExchangePlatform/client/ExchangeClient.java
@@ -2,9 +2,7 @@ package nw.ExchangePlatform.client;
 
 import nw.ExchangePlatform.data.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -23,89 +21,23 @@ public class ExchangeClient {
     }
 
     public void SubmitMarketOrder(Direction direction, String tickerSymbol, int size, OrderDuration orderDuration) {
-        byte[] order = new byte[46];
+        OrderDTO order = new OrderDTO(direction,OrderType.MARKETORDER,tickerSymbol,size,orderDuration);
 
-        byte[] userIDByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(userID).array();
-        //fix name with 8 character max;
-        byte[] nameByte = name.getBytes();
-        byte[] orderIDByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(orderID).array();
-        //convert time to millisecond first and then to byte
-        byte[] timeByte = ByteBuffer.allocate(8).order(ByteOrder.nativeOrder()).putInt(orderID).array();
-        if(direction.equals(Direction.BUY)) {
-            order[24] = (byte) 1;
-        } else {
-            order[24] = (byte) 2;
-        }
-        byte[] tickerSymbolByte = tickerSymbol.getBytes();
-        byte[] sizeByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(size).array();
-        byte[] priceByte = ByteBuffer.allocate(8).order(ByteOrder.nativeOrder()).putDouble(price).array();
-        if(orderType.equals(OrderType.MARKETORDER)) {
-            order[44] = (byte) 1;
-        } else {
-            order[44] = (byte) 2;
-        }
-        if(orderDuration.equals(OrderDuration.DAY)) {
-            order[45] = (byte) 1;
-        } else {
-            order[45] = (byte) 2;
-        }
-        System.arraycopy(userIDByte,0,order,0,4);
-        System.arraycopy(nameByte,0,order,4,8);
-        System.arraycopy(orderIDByte,0,order,12,4);
-        System.arraycopy(timeByte,0,order,16,8);
-        System.arraycopy(tickerSymbolByte,0,order,25,8);
-        System.arraycopy(sizeByte,0,order,32,4);
-        System.arraycopy(priceByte,0,order,36,8);
-
-
-        try {
-            clientSocket.getOutputStream().write(order);
-        } catch (Exception e) {
-            String meg = e.getMessage();
-        }
+        TransmitOrderDTO(order);
     }
 
     public void SubmitLimitOrder(Direction direction, String tickerSymbol, int size, double price, OrderDuration orderDuration) {
+        OrderDTO limitOrder = new OrderDTO(direction,OrderType.LIMITORDER,tickerSymbol,size,price,orderDuration);
+        byte[] limitOrderDTO = limitOrder.Serialize();
+    }
 
-        byte[] order = new byte[46];
-
-        byte[] userIDByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(userID).array();
-        //fix name with 8 character max;
-        byte[] nameByte = name.getBytes();
-        byte[] orderIDByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(orderID).array();
-        //convert time to millisecond first and then to byte
-        byte[] timeByte = ByteBuffer.allocate(8).order(ByteOrder.nativeOrder()).putInt(orderID).array();
-        if(direction.equals(Direction.BUY)) {
-            order[24] = (byte) 1;
-        } else {
-            order[24] = (byte) 2;
-        }
-        byte[] tickerSymbolByte = tickerSymbol.getBytes();
-        byte[] sizeByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(size).array();
-        byte[] priceByte = ByteBuffer.allocate(8).order(ByteOrder.nativeOrder()).putDouble(price).array();
-        if(orderType.equals(OrderType.MARKETORDER)) {
-            order[44] = (byte) 1;
-        } else {
-            order[44] = (byte) 2;
-        }
-        if(orderDuration.equals(OrderDuration.DAY)) {
-            order[45] = (byte) 1;
-        } else {
-            order[45] = (byte) 2;
-        }
-        System.arraycopy(userIDByte,0,order,0,4);
-        System.arraycopy(nameByte,0,order,4,8);
-        System.arraycopy(orderIDByte,0,order,12,4);
-        System.arraycopy(timeByte,0,order,16,8);
-        System.arraycopy(tickerSymbolByte,0,order,25,8);
-        System.arraycopy(sizeByte,0,order,32,4);
-        System.arraycopy(priceByte,0,order,36,8);
-
-
+    private void TransmitOrderDTO (OrderDTO orderDTO) {
+        byte[] orderDTOByteArray = orderDTO.Serialize();
         try {
-            clientSocket.getOutputStream().write(order);
-        } catch (Exception e) {
-            String meg = e.getMessage();
+            OutputStream outputStream = clientSocket.getOutputStream();
+            outputStream.write(orderDTOByteArray);
+        } catch(IOException E) {
+
         }
     }
 
--- src/nw/ExchangePlatform/data/OrderDTO.java
+++ src/nw/ExchangePlatform/data/OrderDTO.java
@@ -1,32 +1,112 @@
 package nw.ExchangePlatform.data;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class OrderDTO {
 
     private final Direction direction;
     private final String tickerSymbol;
     private final int size;
     private final double price;
-    public final OrderDuration orderDuration;
-
+    private final OrderDuration orderDuration;
+    private final OrderType orderType;
+    public byte[] orderDTOByteArray;
     //constructor
     //this constructor is used for limit order data transfer object
-    public OrderDTO(Direction direction, String tickerSymbol, int size, double price, OrderDuration orderDuration)
+    public OrderDTO(Direction direction, OrderType orderType, String tickerSymbol, int size, double price, OrderDuration orderDuration)
     {
         this.tickerSymbol = tickerSymbol;
         this.size = size;
         this.price = price;
         this.direction = direction;
         this.orderDuration = orderDuration;
+        this.orderType = orderType;
     }
 
     //this constructor is used for market order data transfer object
-    public OrderDTO(Direction direction, String tickerSymbol, int size, OrderDuration orderDuration)
+    public OrderDTO(Direction direction, OrderType orderType, String tickerSymbol, int size, OrderDuration orderDuration)
     {
         this.tickerSymbol = tickerSymbol;
         this.size = size;
         this.direction = direction;
         this.orderDuration = orderDuration;
+        this.orderType = orderType;
         this.price = -1;
+    }
+
+    public OrderDTO(byte[] DTOByteArray) {
+        this.orderDTOByteArray = DTOByteArray;
+        int position = 0;
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(DTOByteArray);
+
+        byte[] buffer = new byte[1];
+        inputStream.read(buffer, position, 1);
+        direction = Direction.valueOf(ByteBuffer.wrap(buffer).getInt());
+        position += 1;
+
+        inputStream.read(buffer,position,1);
+        orderType = OrderType.valueOf(ByteBuffer.wrap(buffer).getInt());
+        position += 1;
+
+        inputStream.read(buffer, position, 1);
+        int numOfTickerChar = ByteBuffer.wrap(buffer).getInt();
+        byte[] tickerSymbolBuffer = new byte[numOfTickerChar];
+        inputStream.read(tickerSymbolBuffer, position, numOfTickerChar);
+        position += numOfTickerChar;
+        tickerSymbol = tickerSymbolBuffer.toString();
+
+        byte[] sizeBuffer = new byte[4];
+        inputStream.read(sizeBuffer,position,4);
+        size = ByteBuffer.wrap(buffer).getInt();
+        position += 4;
+
+        if(orderType.equals(OrderType.LIMITORDER)) {
+            byte[] priceBuffer = new byte[8];
+            inputStream.read(priceBuffer, position, 8);
+            price = ByteBuffer.wrap(priceBuffer).getDouble();
+            position += 8;
+        } else {
+            price = -1;
+        }
+
+        inputStream.read(buffer, position, 1);
+        orderDuration = OrderDuration.valueOf(ByteBuffer.wrap(buffer).getInt());
+    }
+
+    public byte[] Serialize()
+    {
+        try {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                outputStream.write(direction.getByteValue());
+
+                outputStream.write(orderType.getByteValue());
+
+                byte[] tickerSymbolByte = tickerSymbol.getBytes();
+                int tickerSymbolByteSize = tickerSymbolByte.length;
+                outputStream.write((byte) tickerSymbolByteSize);
+                outputStream.write(tickerSymbolByte);
+
+                byte[] sizeByte = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder()).putInt(size).array();
+                outputStream.write(sizeByte);
+
+                if(orderType.equals(OrderType.LIMITORDER)) {
+                    byte[] priceByte = ByteBuffer.allocate(8).order(ByteOrder.nativeOrder()).putDouble(price).array();
+                    outputStream.write(priceByte);
+                }
+
+                outputStream.write(orderDuration.getByteValue());
+
+                orderDTOByteArray = outputStream.toByteArray();
+
+            } catch (IOException e) {
+
+            }
+
+        return orderDTOByteArray;
     }
 }
