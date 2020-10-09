078c3f4041728045ee3bae4a9ec760617383a708
NinaWang <niannian1989@gmail.com>
2020-10-08 17:16:45 -0700

    Assertions test

--- IntegrationTest/src/test/java/integration/IntegrationTest.java
+++ IntegrationTest/src/test/java/integration/IntegrationTest.java
@@ -31,7 +31,7 @@ public class IntegrationTest {
         ServerEngine server = new ServerEngine(config);
         server.Start();
 
-        Thread.sleep(3000);
+        Thread.sleep(13000);
     }
 
     @Test
@@ -87,11 +87,11 @@ public class IntegrationTest {
         expectedBids.add(expectedItem2);
 
         //compare results
-        Assertions.assertThat(expectedNumOfAsks).isEqualTo(marketData.getAsks().size());
+     /*   Assertions.assertThat(expectedNumOfAsks).isEqualTo(marketData.getAsks().size());
         Assertions.assertThat(expectedNumOfBids).isEqualTo(marketData.getBids().size());
         Assertions.assertThat(expectedBids).usingRecursiveComparison().isEqualTo(marketData.getBids());
-
-        Thread.sleep(1000);
+*/
+        Thread.sleep(3000);
     }
 
     private void SubmitSerialOrders(Object client1Monitor, Object client2Monitor, Object client3Monitor) throws Exception {
@@ -147,5 +147,6 @@ class TestEventHandler implements OrderStatusEventHandler {
             }
         }
         System.out.print(requestID);
+        Assertions.assertThat(false).isEqualTo(true);
     }
 }
